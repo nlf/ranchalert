f987f61f9e0fec23142a126e260b4907f717ab99
Nathan LaFreniere <quitlahok@gmail.com>
2015-12-18 06:31:10 -0800

    track previous state

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const Config = require('getconfig');
 const WebSocket = require('ws');
 const Wreck = require('wreck');
+const internals = {};
+internals.states = {};
 
 const url = `wss://${Config.accessKey}:${Config.secretKey}@${Config.host}/v1/subscribe?eventNames=resource.change`;
 const socket = new WebSocket(url);
@@ -22,7 +24,16 @@ socket.on('message', (msg) => {
   }
 
   const resource = msg.data.resource;
+  internals.states[resource.type] = internals.states[resource.type] || {};
+  if (internals.states[resource.type][resource.id] === resource.state) {
+    return;
+  }
+
+  internals.states[resource.type][resource.id] = resource.state;
   const states = Config.states[msg.resourceType];
+  console.log(`${new Date().toISOString()} DEBUG: ${msg.resourceType} ${resource.name} is now in the ${resource.state} state`);
+  console.log(require('util').inspect(msg, false, null, true));
+
   if (states.indexOf(resource.state) === -1) {
     return;
   }
@@ -34,17 +45,19 @@ socket.on('message', (msg) => {
     text: `${msg.resourceType} ${resource.name} is now in the ${resource.state} state`
   });
 
-  Wreck.post(Config.slack.url, { payload: payload, json: true }, (err, res, body) => {
+  console.log(`${new Date().toISOString()} Posting ${payload} to slack`);
 
-    if (err ||
-        res.statusCode !== 200) {
+  if (Config.notify) {
+    Wreck.post(Config.slack.url, { payload: payload, json: true }, (err, res, body) => {
 
-      console.log(`${new Date().toISOString()} ERROR failed to post to slack: ${err.message || res.statusCode}`);
-      return;
-    }
+      if (err ||
+          res.statusCode !== 200) {
 
-    console.log(`${new Date().toISOString()} Posted ${payload} to slack`);
-  });
+        console.log(`${new Date().toISOString()} ERROR failed to post to slack: ${err.message || res.statusCode}`);
+        return;
+      }
+    });
+  }
 });
 
 socket.on('close', () => {
