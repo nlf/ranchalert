449d90dc177101dde5eafbb5d3dc27318a51f806
Nathan LaFreniere <quitlahok@gmail.com>
2015-12-17 18:00:21 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const Config = require('getconfig');
+const WebSocket = require('ws');
+const Wreck = require('wreck');
+
+const url = `wss://${Config.accessKey}:${Config.secretKey}@${Config.host}/v1/subscribe?eventNames=resource.change`;
+const socket = new WebSocket(url);
+
+socket.on('open', () => {
+
+  console.log(`${new Date().toISOString()} Socket opened`);
+});
+
+socket.on('message', (msg) => {
+
+  msg = JSON.parse(msg);
+  if (msg.name !== 'resource.change' ||
+      Config.resources.indexOf(msg.resourceType) === -1) {
+
+    return;
+  }
+
+  const resource = msg.data.resource;
+  const states = Config.states[msg.resourceType];
+  if (states.indexOf(resource.state) === -1) {
+    return;
+  }
+
+  const payload = JSON.stringify({
+    username: 'rancher',
+    icon_emoji: ':rancher:',
+    channel: Config.slack.channel,
+    text: `${msg.resourceType} ${resource.name} is now in the ${resource.state} state`
+  });
+
+  Wreck.post(Config.slack.url, { payload: payload, json: true }, (err, res, body) => {
+
+    if (err ||
+        res.statusCode !== 200) {
+
+      console.log(`${new Date().toISOString()} ERROR failed to post to slack: ${err.message || res.statusCode}`);
+      return;
+    }
+
+    console.log(`${new Date().toISOString()} Posted ${payload} to slack`);
+  });
+});
+
+socket.on('close', () => {
+
+  console.log(`${new Date().toISOString()} Socket closed`);
+});
